e819d545e0aa40dd32293c21026500ad89119c31
Aldf76 <contasfelipe623@gmail.com>
2024-11-27 18:27:30 -0300

    Atualizaçção dos estilos de Card e Grid para MUI sx

---
+++ react-carioca-sports-site/src/Moleculas/GridProdutos/GridProdutosStyles.js
@@ -0,0 +1,22 @@
+// GridProdutosStyle.js
+const GridProdutosStyle = {
+    container: {
+      flexGrow: 1,
+      padding: 3,
+      backgroundColor: "#f5f5f5", // Fundo suave
+    },
+    gridItem: {
+      display: "flex",
+      justifyContent: "center", // Centraliza os cards dentro do grid
+      alignItems: "center",
+    },
+    loaderContainer: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      minHeight: "50vh", // Ocupa metade da tela verticalmente enquanto carrega
+    },
+  };
+  
+  export default GridProdutosStyle;
+  
