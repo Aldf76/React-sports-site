1d3a5d37305043b2f7e57b6594e1b6e3fbe70900
Aldf76 <contasfelipe623@gmail.com>
2024-11-27 17:12:29 -0300

    Adição do carouselContainer atualizado e exclusão da sua versão antiga

--- react-carioca-sports-site/src/Moleculas/CarouselContainer/CarouselContainer.js
+++ react-carioca-sports-site/src/Moleculas/CarouselContainer/CarouselContainer.js
@@ -1,15 +1,14 @@
 import React from "react";
-import '../Aula 10/style.css';
-import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
-import { Carousel as ResponsiveCarousel } from 'react-responsive-carousel';
-import Imagem from './camisa-de-time.jpg'
-import Imagem2 from './camisa-de-time2.jpg'
-import Imagem3 from './camisa-de-time3.jfif'
+import "./CarouselContainer.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel as ResponsiveCarousel } from "react-responsive-carousel";
+import Imagem from "../../assets/camisa-de-time.jpg";
+import Imagem2 from "../../assets/camisa-de-time2.jpg";
+import Imagem3 from "../../assets/camisa-de-time3.jfif";
 
 const CarouselContainer = () => {
-
   return (
-    <div className='box_banner'>
+    <div className="box_banner">
       <ResponsiveCarousel
         infiniteLoop={true}
         autoPlay={true}
@@ -28,10 +27,8 @@ const CarouselContainer = () => {
           <img src={Imagem3} alt="Slide 3" />
         </div>
       </ResponsiveCarousel>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
     </div>
   );
-}
+};
 
 export default CarouselContainer;
